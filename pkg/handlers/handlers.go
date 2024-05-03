ba738667cb528da7144989ab861264c1daaf0775
Louis Ikwuegbu <Lou.ikwuegbu@gmail.com>
2024-05-03 14:24:16 +0100

    mopping up git fix

--- pkg/handlers/handlers.go
+++ pkg/handlers/handlers.go
@@ -15,21 +15,3 @@ func About(w http.ResponseWriter, r *http.Request) {
 	render.RenderTemplate(w, "about.page.tmpl")
 
 }
-<<<<<<< HEAD
-
-// func RenderTemplateTest(w http.ResponseWriter, tmpl string) error {
-// 	myCache := map[string]*template.Template{}
-
-// 	pages, err := filepath.Glob("./templates/*.page.tmpl")
-// 	if err != nil {
-// 		return err
-// 	}
-
-// 	for _, page := range pages {
-// 		name := filepath.Base(page)
-
-// 		ts, err := template.New(name).Funcs(functions).ParseFiles(page)
-// 	}
-// }
-=======
->>>>>>> 637c688 (fixing git issues after system crash)
